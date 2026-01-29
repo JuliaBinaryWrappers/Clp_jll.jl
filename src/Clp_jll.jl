37cd5afe1ea2616d55e78ea312d92d645864eccc
jlbuild <juliabuildbot@gmail.com>
2026-01-29 00:50:15 +0000

    Clp_jll build 100.1700.1001+0

--- src/Clp_jll.jl
+++ src/Clp_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("Clp")
-JLLWrappers.@generate_main_file("Clp", UUID("06985876-5285-5a41-9fcb-8948a742cc53"))
+JLLWrappers.@generate_main_file("Clp", Base.UUID("06985876-5285-5a41-9fcb-8948a742cc53"))
 end  # module Clp_jll
